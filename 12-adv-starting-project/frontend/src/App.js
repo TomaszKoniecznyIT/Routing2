7214be81e6b0b9c1a7214b85bee5e537ca1aa1d7
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-10-29 10:59:54 +0000

    dynamic routes and loader()

--- 12-adv-starting-project/frontend/src/App.js
+++ 12-adv-starting-project/frontend/src/App.js
@@ -24,7 +24,9 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
 import EventsPage, { loader as eventLoader } from "./pages/EventsPage";
-import EventDetailPage from "./pages/EventDetailPage";
+import EventDetailPage, {
+  loader as eventDetailLoader,
+} from "./pages/EventDetailPage";
 import NewEventPage from "./pages/NewEventPage";
 import EditEventPage from "./pages/EditEventPage";
 import RootLayout from "./pages/Root";
@@ -47,7 +49,11 @@ const router = createBrowserRouter([
             element: <EventsPage />,
             loader: eventLoader,
           },
-          { path: ":eventId", element: <EventDetailPage /> },
+          {
+            path: ":eventId",
+            element: <EventDetailPage />,
+            loader: eventDetailLoader,
+          },
           { path: "new", element: <NewEventPage /> },
           { path: ":eventId/edit", element: <EditEventPage /> },
         ],
