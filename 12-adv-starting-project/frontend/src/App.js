f32fce44035e4de229d8b58c5a3599666ab3e087
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-11-01 12:13:47 +0000

    useFetcher

--- 12-adv-starting-project/frontend/src/App.js
+++ 12-adv-starting-project/frontend/src/App.js
@@ -22,16 +22,16 @@
 
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import EditEventPage from "./pages/EditEvent";
-import ErrorPage from "./pages/Error";
+import EditEventPage from "./pages/EditEventPage";
+import ErrorPage from "./pages/ErrorPage";
 import EventDetailPage, {
   loader as eventDetailLoader,
   action as deleteEventAction,
-} from "./pages/EventDetail";
-import EventsPage, { loader as eventsLoader } from "./pages/Events";
+} from "./pages/EventDetailPage";
+import EventsPage, { loader as eventsLoader } from "./pages/EventsPage";
 import EventsRootLayout from "./pages/EventsRoot";
-import HomePage from "./pages/Home";
-import NewEventPage from "./pages/NewEvent";
+import HomePage from "./pages/HomePage";
+import NewEventPage from "./pages/NewEventPage";
 import RootLayout from "./pages/Root";
 import { action as manipulateEventAction } from "./components/EventForm";
 import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
